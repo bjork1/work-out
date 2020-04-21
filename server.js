d123013baaaadf3326a47d3ccc0de5268fa8c2c2
Andrew Bjork <bjorkandrew1@gmail.com>
2020-04-21 18:27:30 -0500

    update

--- server.js
+++ server.js
@@ -1,41 +1,29 @@
 const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
-const Workout = require("./models/workout");
-
 let mongoose = require("mongoose");
-let db = require("./models");
-//const PORT = 3000;
-//try:
-//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/3000", {
-//useNewUrlParser: true,
-//useFindAndModify: false,
-//});
-
-mongoose.connect(
-   "mongodb://heroku_qxgp2dv7:t3qn0ktluan9sjv52mharobqgr@ds045714.mlab.com:45714/heroku_qxgp2dv7",
-
-   {
-     useNewUrlParser: true,
-     useFindAndModify: false,
-   }
- );
-
 const PORT = process.env.PORT || 3000;
-
 const app = express();
-
-//app.use(logger("dev"));
-//app.use(express.logger("dev"));
-
-app.use(morgan("tiny"));
-
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const Workout = require("./models/workout.js");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const logger = require("morgan");
+const path = require("path");
+app.use(logger("dev"));
+
+const data = {
+  array: ["item1", "item2", "item3"],
+  boolean: false,
+  string:
+    "\"Don't worry if it doesn't work right. If everything did, you'd be out of a job\" - Mosher's Law of Software Engineering",
+  number: 42,
+};
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/index.html"));
@@ -49,7 +37,28 @@ app.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/stats.html"));
 });
 
-app.get("/api/workouts", async (req, res) => {
+app.post("/api/workouts", ({ workout }, res) => {
+  // const workout = new Workout({ exercises: req.body });
+  Workout.create(workout)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+app.post("/api/workouts/bulk", ({ workout }, res) => {
+  Workout.insertMany(workout)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+app.get("/api/workouts", (req, res) => {
   Workout.find({})
     .then((dbWorkout) => {
       res.json(dbWorkout);
@@ -59,18 +68,7 @@ app.get("/api/workouts", async (req, res) => {
     });
 });
 
-app.post("/api/workouts", async (req, res) => {
-  const workout = new Workout({ exercises: req.body });
-  Workout.create(workout)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-app.put("/api/workouts/:id", async (req, res) => {
+app.put("/api/workouts/:id", (req, res) => {
   const id = req.params.id;
   const data = req.body;
 
@@ -107,6 +105,7 @@ app.get("/api/workouts/range", async (req, res) => {
     });
 });
 
+//app.use(require("./routes/api.js"));
 app.listen(PORT, function () {
   console.log("App now listening at localhost:" + PORT);
 });
